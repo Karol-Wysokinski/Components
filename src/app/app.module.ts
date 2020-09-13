ba8d589ece3d39cbc2ba7fe092837658b892ae8d
Karol Wysokiński <k.j.wysokinski@gmail.com>
2020-09-13 14:21:00 +0200

    bump4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './component/home/home.component';
 import { ContactComponent } from './component/contact/contact.component';
 import { AboutComponent } from './component/about/about.component';
+import { LoooadingComponent } from './component/loooading/loooading.component';
+import { TestComponentComponent } from './component/test-component/test-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     ContactComponent,
-    AboutComponent
+    AboutComponent,
+    LoooadingComponent,
+    TestComponentComponent
   ],
   imports: [
     BrowserModule,
