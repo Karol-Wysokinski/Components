7f649584ad8e78e367608fea34404af0e6339106
Karol Wysokiński <k.j.wysokinski@gmail.com>
2020-09-13 13:10:57 +0200

    Bump

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AboutComponent} from './component/about/about.component';
+import {HomeComponent} from './component/home/home.component';
+import {ContactComponent} from './component/contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
